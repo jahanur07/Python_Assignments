68289f4b9c302e94d8632e466d251a6361d01834
Jahanur Raman <jahanurraman3@gmail.com>
2026-02-13 09:31:36 +0530

    new update

---
+++ Assignment_1/task1.py
@@ -0,0 +1,7 @@
+a=int(input("Enter The First Number: "))
+b=int(input("Enter The second Number: "))
+
+print("Addition: ",(a+b) )
+print("Subtraction: ",(a-b) )
+print("Multiplication: ",(a*b) )
+print("Division: ",(a/b) )
---
+++ Assignment_1/task2.py
@@ -0,0 +1,4 @@
+fname=input("Enter your First name: ")
+lname=input("Enter your last name: ")
+
+print("Hello,"+ fname +" "+lname+"! Wolcome to Python program.")
